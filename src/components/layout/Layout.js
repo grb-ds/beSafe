911ac99c5d49b276bf2c3d11357368d27e1b126d
ZooWie <55379205+DeRooDirk@users.noreply.github.com>
2021-03-19 10:00:35 +0100

    update
    update on bg picture

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,17 +1,17 @@
 import Head  from 'next/head';
 import styles from "./Layout.module.css"
-import { ReactComponent as Logo } from '../../assets/images/super_woman.svg';
+
 
 const Layout = ({children}) => {
     return (
        <div className={styles.container}>
            <div className={styles.left}>
                <header>
-                   <h1>BeSafe</h1>
+                   <h1 className={styles.titel}>BeSafe</h1>
                </header>
-               <section>
-                   <Logo className={styles.logo}/>
-               </section>
+               
+                   
+               
 
            </div>
            <div className={styles.right}>
